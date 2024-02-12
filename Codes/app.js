2b65a7627801cfcc5bb9f524ab3164a0d2365789
Harshit Singla <harshitashwani@gmail.com>
2024-02-12 16:01:08 +0530

    Hamburger made functional

--- Codes/app.js
+++ Codes/app.js
@@ -10,3 +10,16 @@ menuItems.forEach(item => {
         item.classList.remove('inactive');
     });
 });
+// Get a reference to the menu toggle checkbox
+const menuToggle = document.getElementById('menu-toggle');
+
+// Get a reference to the nav element
+const nav = document.querySelector('nav');
+const navIcons = document.querySelectorAll(`nav a`);
+// Add an event listener to the menu toggle checkbox
+menuToggle.addEventListener('change', function() {
+  // Toggle the 'opened' class on the nav element
+  nav.classList.toggle('opened');
+  navIcons.classList.toggle('opened');
+
+});
