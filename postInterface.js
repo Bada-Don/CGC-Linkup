2f294b3b76e6be462687c629b5abe78900f4e40a
Harshit Singla <harshitashwani@gmail.com>
2024-02-25 10:48:43 +0530

    The post like increases count. Other cards haven't got the interface yet.

--- postInterface.js
+++ postInterface.js
@@ -1,13 +1,18 @@
 // Making like button in post functional
 let isLiked = false;
+let likeCount = parseInt(document.getElementById( 'likeCount' ).innerText);
 
 function toggleLike() {
   isLiked = !isLiked;
   const likeIcon = document.getElementById('like-icon');
   if (isLiked) {
     likeIcon.src = 'assets/Icons/Like.png';
+    likeCount++;
+    document.getElementById('likeCount').innerHTML = likeCount;
   } else {
     likeIcon.src = 'assets/Icons/Unlike.png';
+    likeCount--;
+    document.getElementById('likeCount').innerHTML = likeCount;
   }
 }
 // Making comment button in post functional
