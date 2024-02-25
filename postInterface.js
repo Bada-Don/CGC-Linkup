9d86a0454c07d0f795a9ce95a5f195c0184433fe
Harshit Singla <harshitashwani@gmail.com>
2024-02-25 12:04:58 +0530

    Comment count added.

--- postInterface.js
+++ postInterface.js
@@ -1,29 +1,47 @@
+
+// // Making comment button in post functional
+let commentBtns = document.querySelectorAll('.comment-icon');
+
+commentBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        let commentCount = parseInt(this.nextElementSibling.innerText);
+        
+        if (!this.classList.contains('commented')) {
+            this.src = './assets/Icons/Comment_O.png';
+            this.alt = 'comment';
+            this.classList.add('commented');
+            // commentCount++;
+        } else {
+            this.src = './assets/Icons/Comment_C.png';
+            this.alt = 'Like';
+            this.classList.remove('commented');
+            // commentCount--;
+        }
+        
+        this.nextElementSibling.innerText = commentCount;
+    });
+});
+
 // Making like button in post functional
-let isLiked = false;
-let likeCount = parseInt(document.getElementById( 'likeCount' ).innerText);
+let likeBtns = document.querySelectorAll('.like-icon');
 
-function toggleLike() {
-  isLiked = !isLiked;
-  const likeIcon = document.getElementById('like-icon');
-  if (isLiked) {
-    likeIcon.src = 'assets/Icons/Like.png';
-    likeCount++;
-    document.getElementById('likeCount').innerHTML = likeCount;
-  } else {
-    likeIcon.src = 'assets/Icons/Unlike.png';
-    likeCount--;
-    document.getElementById('likeCount').innerHTML = likeCount;
-  }
-}
-// Making comment button in post functional
-let isCommented = false;
+likeBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        let likeCount = parseInt(this.nextElementSibling.innerText);
+        
+        if (!this.classList.contains('liked')) {
+            this.src = './assets/Icons/Like.png';
+            this.alt = 'Liked';
+            this.classList.add('liked');
+            likeCount++;
+        } else {
+            this.src = './assets/Icons/Unlike.png';
+            this.alt = 'Like';
+            this.classList.remove('liked');
+            likeCount--;
+        }
+        
+        this.nextElementSibling.innerText = likeCount;
+    });
+});
 
-function toggleComment() {
-  isCommented = !isCommented;
-  const commentIcon = document.getElementById('comment-icon');
-  if (isCommented) {
-    commentIcon.src = 'assets/Icons/Comment_O.png';
-  } else {
-    commentIcon.src = 'assets/Icons/Comment_C.png';
-  }
-}
